07f310f17bcdd14090a1597b646e54156f7a4fcd
anurag <anurag.botre.202@gmail.com>
2020-12-28 20:55:52 +0530

    Topology. Processors in Kafka Stream.

---
+++ src/main/java/com/trendcore/kafka/Pipe.java
@@ -0,0 +1,53 @@
+package com.trendcore.kafka;
+
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.KStream;
+
+import java.util.Properties;
+import java.util.concurrent.CountDownLatch;
+
+public class Pipe {
+
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(StreamsConfig.APPLICATION_ID_CONFIG,"streams-pipe");
+        properties.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,"localhost:9092");
+        properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+        properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass());
+
+        final StreamsBuilder builder = new StreamsBuilder();
+
+        KStream<String, String> source = builder.stream("streams-plaintext-input");
+
+        source.to("streams-pipe-output");
+
+        Topology topology = builder.build();
+
+        System.out.println(topology.describe());
+
+        KafkaStreams kafkaStreams = new KafkaStreams(topology,properties);
+
+        CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        Runtime.getRuntime().addShutdownHook(new Thread("streams-shutdown-hook"){
+            @Override
+            public void run() {
+                kafkaStreams.close();
+                countDownLatch.countDown();
+            }
+        });
+
+        try {
+            kafkaStreams.start();
+            countDownLatch.await();
+        } catch (InterruptedException e) {
+            System.exit(1);
+        }
+
+    }
+
+}
